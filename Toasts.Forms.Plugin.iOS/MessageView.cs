b4cd0e98bb81b37a639dde03e644777e84922291
Junian <git@junian.net>
2015-09-17 23:22:45 +0700

    Fixed MessageBar height for iOS

--- Toasts.Forms.Plugin.iOS/MessageView.cs
+++ Toasts.Forms.Plugin.iOS/MessageView.cs
@@ -206,7 +206,7 @@ namespace Toasts
 
             CGSize descriptionLabelSize = DescriptionSize();
             DescriptionColor.SetColor();
-            var descriptionRectangle = new RectangleF(xOffset, yOffset, (float)descriptionLabelSize.Width + Padding, (float)descriptionLabelSize.Height);
+			var descriptionRectangle = new RectangleF(xOffset, yOffset, (float)descriptionLabelSize.Width + Padding, (float)descriptionLabelSize.Height+Padding);
             Description.DrawString(descriptionRectangle, DescriptionFont, UILineBreakMode.TailTruncation, UITextAlignment.Left);
         }
 
